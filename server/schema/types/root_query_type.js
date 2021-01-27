c879d5012d09f9bda58cdf75822589bb549f68e2
Ramon Gallo <ramong1145@gmail.com>
2021-01-27 15:07:09 -0600

    ADDED login and logout methods for authentication

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,16 @@
 const graphql = require('graphql');
+const UserType = require('./user_type');
 const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: graphql.GraphQLID }
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
+    }
   }
 });
 
